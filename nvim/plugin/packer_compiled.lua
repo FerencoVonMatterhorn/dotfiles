9b1caf99f50bbe5e3835bc1de6d0a639bd342f3f
Ferenc <ferenc.horvay@de.markant.com>
2022-12-29 11:00:54 +0100

    remove project plugin

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -214,11 +214,6 @@ _G.packer_plugins = {
     path = "/Users/horvayf/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["project.nvim"] = {
-    loaded = true,
-    path = "/Users/horvayf/.local/share/nvim/site/pack/packer/start/project.nvim",
-    url = "https://github.com/ahmedkhalf/project.nvim"
-  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/horvayf/.local/share/nvim/site/pack/packer/start/telescope.nvim",
